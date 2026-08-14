ef2de92d02f566ea37f41151fe011733f05bd77e
reetsharma2020-svg <reet.sharma2020@gmail.com>
2026-08-14 11:29:20 +0530

    Create day1.cpp

---
+++ day1.cpp
@@ -0,0 +1,12 @@
+//Q1: Write a program to input two numbers and display their sum.
+
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("write two numbers\n");
+	scanf("%d %d", &a , &b);
+	c = a+b;
+	printf("The sum is = %d",c);
+	return 0;
+}
