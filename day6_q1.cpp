1095dfa68d01121e17c980ee65eed862180960de
reetsharma2020-svg <reet.sharma2020@gmail.com>
2026-08-16 14:38:21 +0530

    Create day6_q1.cpp

---
+++ day6_q1.cpp
@@ -0,0 +1,12 @@
+//Q11: Write a program to input an integer and check whether it is even or odd using if–else.
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter any number\n");
+	scanf("%d",&a);
+	if (a%2 == 0)
+	printf("It is an even number");
+	else if (printf("it is an odd number"));
+	return 0;
+}
