781b3f589cf77b49c40291020c0185a28566271c
reetsharma2020-svg <reet.sharma2020@gmail.com>
2026-09-06 22:29:53 +0530

    Create day17_q2.cpp

---
+++ day17_q2.cpp
@@ -0,0 +1,19 @@
+//Q34: Write a program to check if a number is prime.
+#include<stdio.h>
+int main()
+{
+	int n,i, count = 0;
+	printf("enter the number to be checked\n");
+	scanf("%d", &n);
+	for (i= 1; i <= n; i++){
+		if (n % i == 0)
+		count ++;
+	}
+	if (count == 2)
+	{
+		printf("Prime number");
+	} else {
+		printf("Not a prime number");
+	}
+	return 0;
+}
