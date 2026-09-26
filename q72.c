18e26539ce6ba7ff6a03ec7cb503fba7bad62ad9
reetsharma2020-svg <reet.sharma2020@gmail.com>
2026-09-26 23:59:44 +0530

    q72

---
+++ q72.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{ int r,c,sum=0;
+printf("Enter the number of row and column:");
+scanf("%d%d",&r,&c);
+int m[r][c];
+printf("Enter the elements of the matrix: ");
+for(int i=0;i<r;i++)
+{
+    for(int j=0;j<c;j++)
+    {
+        scanf("%d", &m[i][j]);
+        sum+=m[i][j];
+    }
+}
+printf("The matrix is:\n");
+for(int i=0;i<r;i++)
+{
+    for(int j=0;j<c;j++)
+    {
+        printf("%d ", m[i][j]);
+    }
+    printf("\n");
+}
+printf("The sum of all elements in the matrix is: %d", sum);
+}
